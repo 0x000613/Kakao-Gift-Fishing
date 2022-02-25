d4789bdcc0fbe99b89b06c5be57402b40bb8ad32
Xeros <xeros.log@gmail.com>
2022-02-26 08:41:47 +0900

    Fix: Coffee.js
    이미지 링크 수정

--- src/Pages/Coffee.js
+++ src/Pages/Coffee.js
@@ -5,7 +5,7 @@ import KakaoShareButton from '../Components/KakaoShareButton'
 const Coffee = () => {
     return (
         <div>
-            <KakaoShareButton thumbnail_URL='https://kakao-gift-fishing.vercel.app/assets/images/maplestory/corejamstone.jpg' giftName='이디야 아이스 아메리카노' buttonID='ediya-ice-americano'/>
+            <KakaoShareButton thumbnail_URL='https://kakao-gift-fishing.vercel.app/assets/images/coffee/ediya-ice-americano.jpg' giftName='이디야 아이스 아메리카노' buttonID='ediya-ice-americano'/>
         </div>
     );
 };
