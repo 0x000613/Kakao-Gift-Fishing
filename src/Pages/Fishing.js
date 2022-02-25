5338f44762756680fdc0d59637176663d528d419
Xeros <xeros.log@gmail.com>
2022-02-26 08:32:56 +0900

    Fix: 문구 수정

--- src/Pages/Fishing.js
+++ src/Pages/Fishing.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 const Fishing = () => {
     return (
         <div>
-            <h1>낚였습니다.</h1>
+            <h1>낚였습니다!</h1>
             <Link to='/'>나도 낚시하러가기</Link>
         </div>
     );
