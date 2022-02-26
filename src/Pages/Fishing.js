c3f932db322636f1834b042db227135ada93d3c5
Xeros <xeros.log@gmail.com>
2022-02-26 22:16:37 +0900

    Update: Fishing 페이지 업데이트

--- src/Pages/Fishing.js
+++ src/Pages/Fishing.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import FishingImage from '../Images/Fishing.png';
+
+import '../CSS/Fishing.css';
 
 const Fishing = () => {
     return (
-        <div>
+        <div className='fishing-container'>
             <h1>낚였습니다!</h1>
-            <Link to='/'>나도 낚시하러가기</Link>
+            <img src={FishingImage} alt='FishingImage' />
+            <Link to='/' className='gotofishing-button'>나도 낚시하러가기 &gt;&gt;</Link>
         </div>
     );
 };
